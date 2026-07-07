5850ad2fedd32e5314886e99a7ba005c611719b7
monkeyhrq <monkeyhrq7@gmail.com>
2026-07-07 19:57:14 +0800

    Time: 3 ms (66.79%), Space: 14.8 MB (37.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-         unordered_map<int, int> seen; // 值 -> 索引
+         unordered_map<int, int> seen; 
         for (int i = 0; i < nums.size(); i++) {
             int need = target - nums[i];
             if (seen.count(need)) {
