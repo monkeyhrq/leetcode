1055965857ffd4274981b4cc2e1c64de9e6fbb90
monkeyhrq <monkeyhrq7@gmail.com>
2026-07-17 19:36:44 +0800

    Time: 0 ms (100%), Space: 71.7 MB (52.67%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int curSum = nums[0];   // 以目前位置結尾的最大和
-        int ans = nums[0];      // 歷史最大值
+        // 一串數字[-2,1,-3,4,-1,2,1,-5,4]，取出相加後最大的數 不能跳著加，要連續
+        //                 [4,-1,2,1]
+        //如果下一個數字+目前數字 比下一個數字低，那我直接選下一個數字
+        //每一次加減完 都把最大的數更新到ans
+        int curSum = nums[0];
+        int ans = nums[0];
 
-        for (int i = 1; i < nums.size(); i++){
-            curSum = max(nums[i], curSum + nums[i]);
+        for(int i = 1; i < nums.size(); i++){
+            curSum = max(nums[i] + curSum, nums[i]);
             ans = max(ans, curSum);
         }
         return ans;
