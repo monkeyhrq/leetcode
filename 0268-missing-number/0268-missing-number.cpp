ef21acd1cbeb2293309a10260a577e8172504e21
monkeyhrq <monkeyhrq7@gmail.com>
2026-07-18 16:08:58 +0800

    Time: 0 ms (100%), Space: 21.9 MB (38.92%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -4,15 +4,14 @@ public:
         //題目給nums陣列，找出裡面缺少的數字
         // n = nums.size()，算出0~n 之間的數字 少了哪一個
 
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
+         int n = nums.size();
+        int ans = 0;
 
-        for(int i = 0; i < n; i++){
-            int count = 0;
-            if (i != nums[i]){
-                return i;
-            }
+        for (int i = 0; i < n; i++){
+            ans = ans ^ i ^ nums[i];   // 索引 0~n-1 和陣列裡的數一起 XOR
         }
-        return n;
+        ans = ans ^ n;                 // 範圍是 0~n,補上最後的 n
+
+        return ans;
     }
 };
